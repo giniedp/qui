d6929682988d4f950fa84d99915a19f0f55b96ef
Alexander Gräfenstein <alex@binarymolecule.com>
2018-11-03 00:22:51 +0100

    typeings and module package.json entries

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import './controls'
 import { Builder } from './controls/builder'
 import { ControlDef, getComponent } from './controls/utils'
 
-export const VERSION = '0.0.2'
+export const VERSION = '0.0.3'
 
 /**
  * Mithril's hyperscript function.
